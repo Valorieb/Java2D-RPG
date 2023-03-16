6de8c842872b7d3b297785083efe9bf4bb792d06
Valorieb <valoriebroderick@gmail.com>
2023-03-16 13:40:33 -0400

    set screen and tile dimensions and created a thread object so that animation can be refreshed

---
+++ src/mainPack/GamePanel.java
@@ -0,0 +1,27 @@
+package mainPack;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel{
+	
+	// SCREEN SETTINGS
+	
+	final int originalTileSize = 16; // 16x16 tile. Common tile size of retro games.
+	final int scale = 3; //3*16 = 48; Resizing for modern, high res screen.
+	
+	final int tileSize = originalTileSize * scale; // 48x48 tile
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+	
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+	}
+
+}
